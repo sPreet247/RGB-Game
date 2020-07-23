542ccf408f496ad55ccd18a5d368518ffa66f047
spreet2472 <samre2472@gmail.com>
2020-07-23 12:24:54 -0400

    New feat added

--- game.js
+++ game.js
@@ -9,10 +9,33 @@ var colors = [
 
 var squares = document.querySelectorAll('.square');
 var colorDisplay = document.querySelector('#colorDisplay');
+var messageDisplay = document.querySelector('#message');
 var pickedColor = colors[3];
 
 colorDisplay.textContent = pickedColor.toUpperCase();
 // Assign colors to squares
 for (var i = 0; i < squares.length; i++) {
   squares[i].style.backgroundColor = colors[i];
+
+  // Add click listeners to all squares
+  squares[i].addEventListener('click', function () {
+    var clickedColor = this.style.backgroundColor;
+
+    // compare the clicked color to the picked colors
+    if (pickedColor === clickedColor) {
+      messageDisplay.textContent = 'Correct';
+      changeColor(pickedColor);
+    } else {
+      this.style.backgroundColor = '#232323';
+      messageDisplay.textContent = 'Try Again';
+    }
+  });
+}
+
+function changeColor(color) {
+  // loop through all squares
+  for (var i = 0; i < squares.length; i++) {
+    // change each color to match pickedColor
+    squares[i].style.backgroundColor = color;
+  }
 }
