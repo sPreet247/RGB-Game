f80c25eb5d2eae96cb1d293860e41d5a93c12d31
spreet2472 <samre2472@gmail.com>
2020-07-25 23:05:29 -0400

    feat random color

--- game.js
+++ game.js
@@ -1,15 +1,9 @@
-var colors = [
-  'rgb(255, 0, 0)',
-  'rgb(255, 255, 0)',
-  'rgb(0, 255, 0)',
-  'rgb(0, 255, 255)',
-  'rgb(0, 0, 255)',
-  'rgb(255, 0, 255)',
-];
+var colors = generateRandomColors(6);
 
 var squares = document.querySelectorAll('.square');
 var colorDisplay = document.querySelector('#colorDisplay');
 var messageDisplay = document.querySelector('#message');
+var h1 = document.querySelector('h1');
 var pickedColor = pickColor();
 
 colorDisplay.textContent = pickedColor.toUpperCase();
@@ -25,6 +19,7 @@ for (var i = 0; i < squares.length; i++) {
     if (pickedColor === clickedColor) {
       messageDisplay.textContent = 'Correct';
       changeColor(pickedColor);
+      h1.style.backgroundColor = clickedColor;
     } else {
       this.style.backgroundColor = '#232323';
       messageDisplay.textContent = 'Try Again';
@@ -44,4 +39,28 @@ function changeColor(color) {
 function pickColor() {
   var random = Math.floor(Math.random() * colors.length);
   return colors[random];
+}
+
+// Generating random colors function
+function generateRandomColors(num) {
+  var arr = [];
+
+  for (var i = 0; i < num; i++) {
+    // get random color and push to arr
+    arr.push(randomColors());
+  }
+
+  return arr;
+}
+
+// rgb random value from 0 - 255 colors
+function randomColors() {
+  // pick red value from 0 - 255
+  var r = Math.floor(Math.random() * 256);
+  // pick green value from 0 - 255
+  var g = Math.floor(Math.random() * 256);
+  // pick blue value from 0 - 255
+  var b = Math.floor(Math.random() * 256);
+  ('rgb(r, g, b)');
+  return 'rgb(' + r + ', ' + g + ', ' + b + ')';
 }
